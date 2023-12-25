05a8e9014af764490b8d9c641e9b886aeaaf090f
fahim <fahim.x@yahoo.com>
2023-12-26 02:18:39 +0600

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+import pyautogui
+import time
+
+pyautogui.press("win")
+
+pyautogui.write("brave")
+
+
+pyautogui.press("enter")
+
+time.sleep(1)
+
+pyautogui.hotkey("Ctrl", "Shift", "N")
+
+pyautogui.write("en.eztv.space")
+
+pyautogui.press("enter")
+
+time.sleep(2)
+
+pyautogui.press("tab")
+
+pyautogui.press("tab")
+
+pyautogui.write("there will be blood")
+
+time.sleep(2)
+
+pyautogui.press("enter")
